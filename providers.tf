9ebe60e6e30bbc194b217bbf2858b3b25ed3c897
docdoom12345 <103731163+docdoom12345@users.noreply.github.com>
2024-03-23 12:06:01 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -16,7 +16,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.94.0"
+      version = "3.0.0"
     }
   }
   #required_version = "=1.7.4"
